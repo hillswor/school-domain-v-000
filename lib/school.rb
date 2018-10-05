2eaf0d8c604dabd352a0ff2906c7c7e13dafdc71
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-10-05 21:59:16 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require "pry"
+
+class School
+
+  attr_reader :school, :roster, :student
+  attr_accessor
+
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !(@roster.include?(grade))
+      @roster[grade] = []
+    end
+    @roster[grade] << student
+  end
+
+  def grade_roster(grade)
+    @roster[grade]
+  end
+
+end
